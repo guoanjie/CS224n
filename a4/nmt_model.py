0c02cb31b464c1441828dd0ded3f72a8feae30ce
Eric Guo <guoanjie@gmail.com>
2019-05-04 23:24:26 -0500

    1e passed

--- a4/nmt_model.py
+++ a4/nmt_model.py
@@ -242,8 +242,14 @@ class NMT(nn.Module):
         ###         https://pytorch.org/docs/stable/torch.html#torch.cat
         ###     Tensor Stacking:
         ###         https://pytorch.org/docs/stable/torch.html#torch.stack
-
-
+        enc_hiddens_proj = self.att_projection(enc_hiddens)
+        Y = self.model_embeddings.target(target_padded)
+        for Y_t in torch.split(Y, 1, dim=0):
+            Ybar_t = torch.cat((torch.squeeze(Y_t), o_prev), dim=1)
+            dec_state, o_t, e_t = self.step(Ybar_t, dec_state, enc_hiddens, enc_hiddens_proj, enc_masks)
+            combined_outputs.append(o_t)
+            o_prev = o_t
+        combined_outputs = torch.stack(combined_outputs)
         ### END YOUR CODE
 
         return combined_outputs
