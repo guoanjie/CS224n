ac37be62d33cecd6693ab1269f779cd798efb5cf
Eric Guo <guoanjie@gmail.com>
2019-05-04 20:37:28 -0500

    implement pad sents

--- a4/utils.py
+++ a4/utils.py
@@ -29,8 +29,8 @@ def pad_sents(sents, pad_token):
     sents_padded = []
 
     ### YOUR CODE HERE (~6 Lines)
-
-
+    max_len = max(len(sent) for sent in sents)
+    sents_padded = [sent + [pad_token] * (max_len - len(sent)) for sent in sents]
     ### END YOUR CODE
 
     return sents_padded
